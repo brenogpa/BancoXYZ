6e2cb0109f0d8fc9d636032ca24b9c478d21ebf4
Breno Abreu <brenogpa@gmail.com>
2024-12-18 20:29:45 -0300

    Adicionar arquivos de projeto.

---
+++ BancoXYZ/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BancoXYZ
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
