46b0ae7fb552d8e94ca1d0be51ad607f93e86247
Breno Abreu <brenogpa@gmail.com>
2024-12-19 15:58:25 -0300

    Creating Registration
    - Created RegisterView for new accounts
    - Created RegisterViewModels for register logic

---
+++ BancoXYZ/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using BancoXYZ.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+
+namespace BancoXYZ.ViewModels
+{
+    public class RegisterViewModel
+    {
+        private readonly UserService _userService;
+
+        public RegisterViewModel()
+        {
+            _userService = new UserService();
+        }
+
+        public void Register(string currentAccount, string password)
+        {
+            if (string.IsNullOrEmpty(currentAccount) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("All fields are required.");
+                return;
+            }
+
+            var users = _userService.LoadUsers();
+            if (users.ContainsKey(currentAccount))
+            {
+                MessageBox.Show("Account already exists.");
+                return;
+            }
+
+            users[currentAccount] = _userService.HashPassword(password);
+            SaveUsers(users);
+            MessageBox.Show("Account created.");
+        }
+
+        private void SaveUsers(Dictionary<string, string> users)
+        {
+            string json = JsonSerializer.Serialize(users);
+            File.WriteAllText(UserService.UsersFilePath, json);
+        }
+    }
+}
---
+++ BancoXYZ/Views/RegisterView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using BancoXYZ.ViewModels;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BancoXYZ.Views
+{
+    public partial class RegisterView : Window
+    {
+        private readonly RegisterViewModel _viewModel;
+
+        public RegisterView()
+        {
+            InitializeComponent();
+            _viewModel = new RegisterViewModel();
+        }
+
+        private void OnRegisterClick(object sender, RoutedEventArgs e)
+        {
+            string currentAccount = CurrentAccountTextBox.Text;
+            string password = PasswordTextBox.Password;
+            _viewModel.Register(currentAccount, password);
+        }
+
+        private void OnBackToLoginClick(object sender, RoutedEventArgs e)
+        {
+            MainView loginView = new MainView();
+            loginView.Show();
+            this.Close();
+        }
+
+
+        private void CurrentAccountTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextNumeric(e.Text);
+        }
+
+        private static bool IsTextNumeric(string text)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            return !regex.IsMatch(text);
+        }
+    }
+}
