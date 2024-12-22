da8d53656f58a9396dcd8f2360d37119d92fad8d
Breno Abreu <brenogpa@gmail.com>
2024-12-22 09:35:49 -0300

    creating unit tests
    - created unit tests for deposit and withdraw methods

---
+++ Tests/ViewModelsTests/HomeViewModelTests.cs
@@ -0,0 +1,45 @@
+﻿using BancoXYZ.Models;
+using BancoXYZ.ViewModels;
+using Xunit;
+
+namespace Tests.ViewModels
+{
+    public class HomeViewModelTests
+    {
+        [Fact]
+        public void Deposit_ShouldIncreaseBalance()
+        {
+            var user = new User { Balance = 100 };
+            var viewModel = new HomeViewModel(user);
+
+            viewModel.Deposit(50);
+
+            Assert.Equal(150, viewModel.Balance);
+        }
+
+        [Fact]
+        public void Withdraw_ShouldDecreaseBalance()
+        {
+            var user = new User { Balance = 100 };
+            var viewModel = new HomeViewModel(user);
+
+            viewModel.Withdraw(50);
+
+            Assert.Equal(50, viewModel.Balance);
+        }
+
+        [Fact]
+        public void Withdraw_ShouldNotAllowOverdraft()
+        {
+            var user = new User { Balance = 100 };
+            var viewModel = new HomeViewModel(user);
+
+            viewModel.Withdraw(150);
+
+            Assert.Equal(100, viewModel.Balance);
+        }
+
+    }
+}
+
+
